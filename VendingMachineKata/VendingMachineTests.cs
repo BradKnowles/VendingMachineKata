3796e45de1d2df8634de2b0b22ca650c647229bf
Brad Knowles <code@bradknowles.com>
2016-04-09 18:25:24 -0400

    Failing Test: Insert a valid coin

---
+++ VendingMachineKata/VendingMachineTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace VendingMachineKata
+{
+    public class VendingMachineTests
+    {
+        public class AcceptCoins
+        {
+            [Fact]
+            public void InsertCoin_InsertValidCoin_UpdatesTotalReturnsTotalForDisplay()
+            {
+                var validCoin = new Coin();
+                var sut = new VendingMachine();
+                var results = sut.InsertCoin(validCoin);
+
+                Assert.Equal("$0.05", result);
+            }
+        }
+
+    }
+}
