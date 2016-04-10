e15b48615c4a0d40462313efd26dfae80ca69705
Brad Knowles <code@bradknowles.com>
2016-04-10 14:37:20 -0400

    Passing Test: Making change from candy or chips purchases

--- VendingMachineKata/MakeChangeTests.cs
+++ VendingMachineKata/MakeChangeTests.cs
@@ -24,6 +24,40 @@ namespace VendingMachineKata
                 Assert.Equal("INSERT COINS", sut.Display);
                 Assert.Equal(0m, sut.Total);
             }
+
+            [Fact]
+            public void CandyButtonPress_UsingMoreThanCorrectChange_DispensesProduct_ReturnsRemainingAmount()
+            {
+                var sut = new VendingMachine();
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.PushCandyButton();
+
+                Assert.Equal("THANK YOU", sut.Display);
+                Assert.Equal(Products.Candy, sut.ProductTray);
+                Assert.Equal(0.35m, sut.CoinReturnTotal);
+                Assert.Equal("INSERT COINS", sut.Display);
+                Assert.Equal(0m, sut.Total);
+            }
+
+            [Fact]
+            public void ChipsButtonPress_UsingMoreThanCorrectChange_DispensesProduct_ReturnsRemainingAmount()
+            {
+                var sut = new VendingMachine();
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Dime);
+                sut.InsertCoin(Coins.Dime);
+                sut.InsertCoin(Coins.Dime);
+                sut.PushChipsButton();
+
+                Assert.Equal("THANK YOU", sut.Display);
+                Assert.Equal(Products.Chips, sut.ProductTray);
+                Assert.Equal(0.05m, sut.CoinReturnTotal);
+                Assert.Equal("INSERT COINS", sut.Display);
+                Assert.Equal(0m, sut.Total);
+            }
         }
     }
 }
