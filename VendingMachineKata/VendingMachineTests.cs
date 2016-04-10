1f8a530324a2ba743371f587eceb8db9b9b0e477
Brad Knowles <code@bradknowles.com>
2016-04-10 00:56:59 -0400

    Failing Test: Selecting cola with correct change

--- VendingMachineKata/VendingMachineTests.cs
+++ VendingMachineKata/VendingMachineTests.cs
@@ -98,25 +98,51 @@ namespace VendingMachineKata
                 };
                 return coins;
             }
+        }
 
-            private static class Coins
+        public class SelectProduct
+        {
+            [Fact]
+            public void ButtonPress_InsertCoinsForProduct_DispenseProduct_DisplaysThankYou()
             {
-                // US Coin Information - https://www.usmint.gov/about_the_mint/?action=coin_specifications
-                // Candian Coin Information - http://www.mint.ca/store/mint/about-the-mint/canadian-circulation-1100028
-                public static Coin Penny => new Coin(2.5m, 19.05m);
-                public static Coin Nickel => new Coin(5m, 21.21m);
-                public static Coin Dime => new Coin(2.268m, 17.91m);
-                public static Coin Quarter => new Coin(5.670m, 24.26m);
-                public static Coin HalfDollar => new Coin(11.340m, 30.61m);
-                public static Coin PresidentialDollar => new Coin(8.1m, 26.49m);
-                public static Coin CanadianPenny => new Coin(2.35m, 19.05m);
-                public static Coin CanadianNickel => new Coin(3.95m, 21.2m);
-                public static Coin CanadianDime => new Coin(1.75m, 18.03m);
-                public static Coin CanadianQuarter => new Coin(4.4m, 23.88m);
-                public static Coin CanadianHalfDollar => new Coin(6.9m, 27.13m);
-                public static Coin CanadianDollar => new Coin(6.27m, 26.5m);
-                public static Coin CanadianTwoDollar => new Coin(6.92m, 28m);
+                var sut = new VendingMachine();
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.PushColaButton();
+                
+                Assert.Equal(sut.Display, "THANK YOU");
+                Assert.Equal(Products.Cola, sut.ProductTray);
             }
         }
+
+        private static class Coins
+        {
+            // US Coin Information - https://www.usmint.gov/about_the_mint/?action=coin_specifications
+            // Candian Coin Information - http://www.mint.ca/store/mint/about-the-mint/canadian-circulation-1100028
+            public static Coin Penny => new Coin(2.5m, 19.05m);
+            public static Coin Nickel => new Coin(5m, 21.21m);
+            public static Coin Dime => new Coin(2.268m, 17.91m);
+            public static Coin Quarter => new Coin(5.670m, 24.26m);
+            public static Coin HalfDollar => new Coin(11.340m, 30.61m);
+            public static Coin PresidentialDollar => new Coin(8.1m, 26.49m);
+            public static Coin CanadianPenny => new Coin(2.35m, 19.05m);
+            public static Coin CanadianNickel => new Coin(3.95m, 21.2m);
+            public static Coin CanadianDime => new Coin(1.75m, 18.03m);
+            public static Coin CanadianQuarter => new Coin(4.4m, 23.88m);
+            public static Coin CanadianHalfDollar => new Coin(6.9m, 27.13m);
+            public static Coin CanadianDollar => new Coin(6.27m, 26.5m);
+            public static Coin CanadianTwoDollar => new Coin(6.92m, 28m);
+        }
+
+        private static class Products
+        {
+            public static Product Cola => new Product();
+        }
+    }
+
+    public class Product
+    {
     }
 }
