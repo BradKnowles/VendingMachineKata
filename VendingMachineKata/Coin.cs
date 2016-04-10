b4de12e2ca2522874c619a09ecacbfeacbd5d6de
Brad Knowles <code@bradknowles.com>
2016-04-09 22:02:26 -0400

    Refactor: Add logic for valid coins and equality
    Removed hardcoding of InsertCoin and added validCoins to set which coins
    are allowed.  Added coinValueMapping to to map coin sizes / diameters to
    their monetary values.  Next step, add and refactor tests to completely
    validate logic.

--- VendingMachineKata/Coin.cs
+++ VendingMachineKata/Coin.cs
@@ -4,13 +4,44 @@ namespace VendingMachineKata
 {
     public class Coin
     {
-        public Decimal WeightInGrams { get; set; }
-        public Decimal DiameterinMillimeters { get; set; }
+        public Decimal WeightInGrams { get; }
+        public Decimal DiameterinMillimeters { get; }
 
         public Coin(Decimal weightInGrams, Decimal diameterinMillimeters)
         {
             WeightInGrams = weightInGrams;
             DiameterinMillimeters = diameterinMillimeters;
+        }
+
+        protected Boolean Equals(Coin other)
+        {
+            return WeightInGrams == other.WeightInGrams && DiameterinMillimeters == other.DiameterinMillimeters;
+        }
+
+        public override Boolean Equals(Object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Coin) obj);
+        }
+
+        public override Int32 GetHashCode()
+        {
+            unchecked
+            {
+                return (WeightInGrams.GetHashCode()*397) ^ DiameterinMillimeters.GetHashCode();
+            }
+        }
+
+        public static Boolean operator ==(Coin left, Coin right)
+        {
+            return Equals(left, right);
+        }
+
+        public static Boolean operator !=(Coin left, Coin right)
+        {
+            return !Equals(left, right);
         }
     }
 }
--- VendingMachineKata/VendingMachine.cs
+++ VendingMachineKata/VendingMachine.cs
@@ -1,26 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace VendingMachineKata
 {
     public class VendingMachine
     {
-        public void InsertCoin(Coin coin)
+        private IReadOnlyList<Coin> _validCoins;
+        private IReadOnlyDictionary<Coin, Decimal> _coinValueMapping;
+
+        // US coin values obtained from https://www.usmint.gov/about_the_mint/?action=coin_specifications
+        private static readonly Coin _nickel = new Coin(5m, 21.21m);
+        private static readonly Coin _dime = new Coin(2.268m, 17.91m);
+        private static readonly Coin _quarter = new Coin(5.670m, 24.26m);
+
+        public VendingMachine()
         {
-            if (coin.WeightInGrams == 2.5m && coin.DiameterinMillimeters == 19.05m)
-            {
-                Display = "$0.00";
-                CoinReturn = coin;
-                Total = 0m;
-            }
-            else
-            {
-                Display = "$0.05";
-                Total = .05m;
-            }
+            _validCoins = SetupValidCoins();
+            _coinValueMapping = MapCoinValues();
         }
 
-        public String Display { get; set; }
+        public void InsertCoin(Coin coin)
+        {
+
+            if (!_validCoins.Contains(coin))
+            {
+                CoinReturn = coin;
+                return;
+            }
+
+            Total = _coinValueMapping[coin];
+        }
+
+        public String Display => Total.ToString("C2");
+
         public Decimal Total { get; set; }
+
         public Coin CoinReturn { get; set; }
+
+        private static IReadOnlyList<Coin> SetupValidCoins()
+        {
+            var validCoins = new List<Coin>
+            {
+                _nickel,
+                _dime,
+                _quarter
+            };
+
+            IReadOnlyList<Coin> readOnlyValidCoins = validCoins;
+            return readOnlyValidCoins;
+        }
+
+        private static IReadOnlyDictionary<Coin, Decimal> MapCoinValues()
+        {
+            var values = new Dictionary<Coin, Decimal>
+            {
+                { _nickel, 0.05m},
+                { _dime, 0.10m},
+                { _quarter, 0.25m}
+            };
+            return new ReadOnlyDictionary<Coin, Decimal>(values);
+        }
     }
 }
