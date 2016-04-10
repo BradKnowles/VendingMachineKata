48c161568a869b03daabe95eab85b7f8b4ebed28
Brad Knowles <code@bradknowles.com>
2016-04-10 15:29:19 -0400

    Passing Test: Added test to verify type and number of coins returned.
    The previous test didn't account for a customer entering multiple quarters and getting back the
    same number of quarters. It just checked that a quarter was returned. This test ensures the customer
    will get back the same number and type of coins they entered. Short changing the customer would be bad :)

--- VendingMachineKata/ReturnCoinsTests.cs
+++ VendingMachineKata/ReturnCoinsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
 
 namespace VendingMachineKata
 {
@@ -15,12 +16,35 @@ namespace VendingMachineKata
                 sut.InsertCoin(Coins.Dime);
 
                 sut.ReturnCoins();
-                
+
                 Assert.Contains(Coins.Quarter, sut.CoinReturnSlot);
                 Assert.Contains(Coins.Nickel, sut.CoinReturnSlot);
                 Assert.Contains(Coins.Dime, sut.CoinReturnSlot);
                 Assert.Equal("INSERT COINS", sut.Display);
+            }
 
+            [Fact]
+            public void ReturnCoinPressed_WhenCustomerInsertsMultipleSimiliarCoins_ReturnsCorrectNumberAndTypeOfCoins_DisplaysInsertCoins()
+            {
+                var sut = new VendingMachine();
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Nickel);
+                sut.InsertCoin(Coins.Nickel);
+                sut.InsertCoin(Coins.Nickel);
+                sut.InsertCoin(Coins.Dime);
+                sut.InsertCoin(Coins.Dime);
+
+                sut.ReturnCoins();
+
+                Assert.Contains(Coins.Quarter, sut.CoinReturnSlot);
+                Assert.Contains(Coins.Nickel, sut.CoinReturnSlot);
+                Assert.Contains(Coins.Dime, sut.CoinReturnSlot);
+                Assert.Equal(7, sut.CoinReturnSlot.Count());
+                Assert.Equal(2, sut.CoinReturnSlot.Count(x => x == Coins.Quarter));
+                Assert.Equal(3, sut.CoinReturnSlot.Count(x => x == Coins.Nickel));
+                Assert.Equal(2, sut.CoinReturnSlot.Count(x => x == Coins.Dime));
+                Assert.Equal("INSERT COINS", sut.Display);
             }
         }
     }
