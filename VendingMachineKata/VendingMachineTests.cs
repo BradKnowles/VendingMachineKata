5a3c3aa0976f2b7c86cf00644114308e123cc970
Brad Knowles <code@bradknowles.com>
2016-04-09 20:24:48 -0400

    Passing Test: Inserting a penny

--- VendingMachineKata/VendingMachineTests.cs
+++ VendingMachineKata/VendingMachineTests.cs
@@ -10,7 +10,7 @@ namespace VendingMachineKata
             [Fact]
             public void InsertCoin_InsertNickel_SetsTotal_SetsDisplay()
             {
-                var nickel = new Coin();
+                var nickel = new Coin(5m, 21.21m);
                 var sut = new VendingMachine();
                 sut.InsertCoin(nickel);
                 Assert.Equal("$0.05", sut.Display);
@@ -20,7 +20,7 @@ namespace VendingMachineKata
             [Fact]
             public void InsertCoin_InsertPenny_DoesNotSetTotal_SendsToCoinReturn()
             {
-                var penny = new Coin();
+                var penny = new Coin(2.5m, 19.05m);
                 var sut = new VendingMachine();
                 sut.InsertCoin(penny);
                 Assert.Equal("$0.00", sut.Display);
@@ -28,19 +28,35 @@ namespace VendingMachineKata
                 Assert.Equal(penny, sut.CoinReturn);
             }
         }
-
     }
 
     public class Coin
     {
+        public Decimal WeightInGrams { get; set; }
+        public Decimal DiameterinMillimeters { get; set; }
+
+        public Coin(Decimal weightInGrams, Decimal diameterinMillimeters)
+        {
+            WeightInGrams = weightInGrams;
+            DiameterinMillimeters = diameterinMillimeters;
+        }
     }
 
     public class VendingMachine
     {
         public void InsertCoin(Coin coin)
         {
-            Display = "$0.05";
-            Total = .05m;
+            if (coin.WeightInGrams == 2.5m && coin.DiameterinMillimeters == 19.05m)
+            {
+                Display = "$0.00";
+                CoinReturn = coin;
+                Total = 0m;
+            }
+            else
+            {
+                Display = "$0.05";
+                Total = .05m;
+            }
         }
 
         public String Display { get; set; }
