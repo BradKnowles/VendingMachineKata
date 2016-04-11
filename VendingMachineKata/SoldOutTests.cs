ee43548c677a10aa6432715dac1b1a17a6ec62fd
Brad Knowles <code@bradknowles.com>
2016-04-10 20:26:06 -0400

    Passing Test: purchasing sold out candy or chips

--- VendingMachineKata/SoldOutTests.cs
+++ VendingMachineKata/SoldOutTests.cs
@@ -17,6 +17,30 @@ namespace VendingMachineKata
                 Assert.Equal("SOLD OUT", sut.Display);
                 Assert.Equal("INSERT COINS", sut.Display);
             }
+
+            [Fact]
+            public void CandyButtonPress_NoCoinsInserted_WithCandySoldOut_DisplaySoldOutThenInsertCoins()
+            {
+                var sut = new VendingMachine();
+                sut.UpdateProductStatus("Candy", "SOLD OUT");
+
+                sut.PushCandyButton();
+
+                Assert.Equal("SOLD OUT", sut.Display);
+                Assert.Equal("INSERT COINS", sut.Display);
+            }
+
+            [Fact]
+            public void ChipsButtonPress_NoCoinsInserted_WithChipsSoldOut_DisplaySoldOutThenInsertCoins()
+            {
+                var sut = new VendingMachine();
+                sut.UpdateProductStatus("Chips", "SOLD OUT");
+
+                sut.PushChipsButton();
+
+                Assert.Equal("SOLD OUT", sut.Display);
+                Assert.Equal("INSERT COINS", sut.Display);
+            }
         }
     }
 }
