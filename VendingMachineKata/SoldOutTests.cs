ab04ad5bcdf453148ae4874c71964749690da51d
Brad Knowles <code@bradknowles.com>
2016-04-10 20:30:33 -0400

    Passing test puchasing cola canday or chips with money already inserted

--- VendingMachineKata/SoldOutTests.cs
+++ VendingMachineKata/SoldOutTests.cs
@@ -41,6 +41,54 @@ namespace VendingMachineKata
                 Assert.Equal("SOLD OUT", sut.Display);
                 Assert.Equal("INSERT COINS", sut.Display);
             }
+
+            [Fact]
+            public void ColaButtonPress_CoinsAlreadyInserted_WithColaSoldOut_DisplaySoldOutThenTotalInserted()
+            {
+                var sut = new VendingMachine();
+                sut.UpdateProductStatus("Cola", "SOLD OUT");
+
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.PushColaButton();
+
+                Assert.Equal("SOLD OUT", sut.Display);
+                Assert.Equal("$1.00", sut.Display);
+            }
+
+            [Fact]
+            public void CandyButtonPress_CoinsAlreadyInserted_WithCandySoldOut_DisplaySoldOutThenTotalInserted()
+            {
+                var sut = new VendingMachine();
+                sut.UpdateProductStatus("Candy", "SOLD OUT");
+
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.PushCandyButton();
+
+                Assert.Equal("SOLD OUT", sut.Display);
+                Assert.Equal("$1.00", sut.Display);
+            }
+
+            [Fact]
+            public void ChipsButtonPress_CoinsAlreadyInserted_WithChipsSoldOut_DisplaySoldOutThenTotalInserted()
+            {
+                var sut = new VendingMachine();
+                sut.UpdateProductStatus("Chips", "SOLD OUT");
+
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.InsertCoin(Coins.Quarter);
+                sut.PushChipsButton();
+
+                Assert.Equal("SOLD OUT", sut.Display);
+                Assert.Equal("$1.00", sut.Display);
+            }
         }
     }
 }
