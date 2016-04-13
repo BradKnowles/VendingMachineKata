e274e90063f4068f6606f18c41974201ace110ef
Brad Knowles <code@bradknowles.com>
2016-04-12 22:45:17 -0400

    Passing Test: Testing default state of DisplayModule

---
+++ VendingMachineKata.Tests.Unit/DisplayModuleTests.cs
@@ -0,0 +1,30 @@
+﻿using VendingMachineKata.Modules;
+using Xunit;
+
+namespace VendingMachineKata.Tests.Unit
+{
+    public class DisplayModuleTests
+    {
+        [Fact]
+        public void DefaultState_NoCoins_Inserted_DisplayInsertCoins()
+        {
+            var sut = new DisplayModule();
+
+            sut.DefaultState();
+
+            Assert.Equal("INSERT COINS", sut.ReadOut);
+        }
+
+        [Fact]
+        public void DefaultState_CoinsInserted_DisplayTotalValue()
+        {
+
+            var sut = new DisplayModule();
+
+            sut.UpdateInsertedCoinValue(0.15m);
+            sut.DefaultState();
+
+            Assert.Equal("$0.15", sut.ReadOut);
+        }
+    }
+}
